accf09504d48e40e649c7b0d3867ee919efb5782
Whenqi <qidewenwhen@gmail.com>
2017-11-01 12:56:24 -0700

    Gulp Part 2

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,14 @@
 
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
+    del = require('del'),
+    imagemin = require('gulp-imagemin'),
+    uglify = require('gulp-uglify'),
+    usemin = require('gulp-usemin'),
+    rev = require('gulp-rev'),
+    cleanCss = require('gulp-clean-css'),
+    flatmap = require('gulp-flatmap'),
+    htmlmin = require('gulp-htmlmin'),
     browserSync = require('browser-sync');
 
 gulp.task('sass', function () {
@@ -34,4 +42,42 @@ gulp.task('browser-sync', function () {
 // Default task Make sure that the browser-sync runs befor the watch task.
 gulp.task('default', ['browser-sync'], function() {
     gulp.start('sass:watch');
+});
+
+// Clean  
+// With the "stream pipe" in gulp we don't need to copy as many files as in the grunt and the npm script.
+gulp.task('clean', function() {
+    return del(['dist']);
+});
+
+gulp.task('copyfonts', function() {
+   gulp.src('./node_modules/font-awesome/fonts/**/*.{ttf,woff,eof,svg}*')
+   .pipe(gulp.dest('./dist/fonts'));
+});
+
+// Images
+gulp.task('imagemin', function() {
+    return gulp.src('img/*.{png,jpg,gif}')
+      .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+      .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('usemin', function() {
+    return gulp.src('./*.html')
+    // The flatmap is used to parallelly execute every html with each pipe or stream.
+    .pipe(flatmap(function(stream, file){
+        return stream
+          .pipe(usemin({
+              css: [ rev() ], //The rev is used for version extension.
+              html: [ function() { return htmlmin({ collapseWhitespace: true })} ],
+              js: [ uglify(), rev() ],
+              inlinejs: [ uglify() ],
+              inlinecss: [ cleanCss(), 'concat' ]
+          }))
+      }))
+      .pipe(gulp.dest('dist/'));
+  });
+
+gulp.task('build',['clean'],function(){ //clean task must be executed before other tasks.
+    gulp.start('copyfonts','imagemin','usemin'); //in start() all tasks will be executed parallely.
 });
